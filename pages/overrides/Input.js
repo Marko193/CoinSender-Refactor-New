efa8f61d6b94c2a34ab62fe4001fa6f283614caa
anton-ishchenkoo <antonishchenko@megadevllc.com>
2023-01-24 15:32:40 +0200

    select fixes

--- pages/overrides/Input.js
+++ pages/overrides/Input.js
@@ -54,8 +54,6 @@ export default function Input(theme) {
     MuiOutlinedInput: {
       styleOverrides: {
         root: {
-          // height: "35px",
-
           fontSize: "14px",
 
           "& .MuiOutlinedInput-notchedOutline": {},
@@ -72,7 +70,6 @@ export default function Input(theme) {
       styleOverrides: {
         root: {},
         popper: {
-          // display: "none",
           boxShadow:
             "0px 5px 5px -3px rgb(0 0 0 / 20%), 0px 8px 10px 1px rgb(0 0 0 / 14%), 0px 3px 14px 2px rgb(0 0 0 / 12%)",
           borderRadius: "8px",
@@ -86,7 +83,6 @@ export default function Input(theme) {
     MuiMenuItem: {
       styleOverrides: {
         root: {
-          // display: "none",
           "&.Mui-selected": {
             background: "#00799414",
             "&:hover": {
