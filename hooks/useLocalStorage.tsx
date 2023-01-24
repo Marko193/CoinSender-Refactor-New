55babaddd840d17d06626a67829d9a0fad593e03
Andrew Marushko <andrewmarushko@gmail.com>
2023-01-24 17:00:58 +0100

    fix window undefined warning

--- hooks/useLocalStorage.tsx
+++ hooks/useLocalStorage.tsx
@@ -1,17 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 interface LocalStorageItem<T> {
   key: string;
   value: T;
 }
 
-const useLocalStorage = <T,>(
-  key: string,
-  initialValue: T
-): [T, (value: T) => void] => {
+const useLocalStorage = <T,>(key: string, initialValue: T): [T, (value: T) => void] => {
   const [storedValue, setStoredValue] = useState<T>(() => {
     try {
-      const item = window.localStorage.getItem(key);
+      const item = localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
       console.log(error);
@@ -21,10 +18,9 @@ const useLocalStorage = <T,>(
 
   const setValue = (value: T) => {
     try {
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value;
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
       setStoredValue(valueToStore);
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      localStorage.setItem(key, JSON.stringify(valueToStore));
     } catch (error) {
       console.log(error);
     }
@@ -36,9 +32,9 @@ const useLocalStorage = <T,>(
         setStoredValue(JSON.parse(e.newValue!));
       }
     };
-    window.addEventListener("storage", onStorageChange);
+    addEventListener('storage', onStorageChange);
     return () => {
-      window.removeEventListener("storage", onStorageChange);
+      removeEventListener('storage', onStorageChange);
     };
   }, [key]);
 
