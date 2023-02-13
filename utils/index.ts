7239610c006e2f9fc7c803bb9857188a2132ec04
Oleksandr <zeppelinua88@gmail.com>
2023-02-13 11:13:16 +0200

    fix

--- utils/index.ts
+++ utils/index.ts
@@ -120,7 +120,7 @@ export function getNonHumanValue(value: number | string, decimals: number): stri
   return parseUnits(value.toString(), decimals).toString();
 }
 
-export function getNonHumanValueSumm(amounts: string[] | BigNumberETH[]): BigNumberETH {
+export function getNonHumanValueSumm(amounts: string[]): BigNumberETH {
   return amounts.reduce((acc: BigNumberETH, amount: string) => {
     return BigNumberETH.from(acc).add(BigNumberETH.from(amount));
   }, BigNumberETH.from(0));
