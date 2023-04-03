7264c372936a8505c491a4d4fceccc74cc7a8149
Marko Pavlenko <mark.pavlenko@megadevllc.com>
2023-04-03 23:14:25 +0300

    feat: added sign up api mechanism

--- components/signUpForm/index.tsx
+++ components/signUpForm/index.tsx
@@ -1,20 +1,24 @@
-import React, {useState} from 'react';
-import {Form, FormikProvider, useFormik} from 'formik';
+import React, { useState } from 'react';
+import { Form, FormikProvider, useFormik } from 'formik';
 // import {Link as RouterLink, useNavigate} from 'react-router-dom';
-import {connect, useSelector} from 'react-redux';
-import {Button, IconButton, InputAdornment, Link, Stack, TextField, Typography,} from '@mui/material';
+import { Button, IconButton, InputAdornment, Link, Stack, TextField, Typography } from '@mui/material';
 // import {LoadingButton} from '@mui/lab';
-import {initialValuesForCompany, validationSchemaForCompany} from '@/constants/registerForm';
+import { initialValuesForCompany, validationSchemaForCompany } from '@/constants/registerForm';
 // import {registerUser} from '../../../redux/actions';
 // import {SIGN_IN} from '../../../constants/routes';
-// import {useTranslation} from 'react-i18next';
 import Iconify from '@/components/iconify';
-// import styled from "styled-components";
+import { signUp } from '@/services';
+import { useRouter } from 'next/router';
 
 export default function RegisterForm() {
+
+  const router = useRouter();
+
   const [showPassword, setShowPassword] = useState(false);
   const [showSecondPassword, setShowSecondPassword] = useState(false);
 
+  const errorMessage = '';
+  // const [errorMessage, setErrorMessage] = useState('');
   // const errorMessage = useSelector(({AuthUser: {errorMessage}}) => errorMessage);
 
   // const navigate = useNavigate();
@@ -30,8 +34,34 @@ export default function RegisterForm() {
     initialValues: initialValuesForCompany,
     validationSchema: validationSchemaForCompany,
 
-    onSubmit: (values) => {
-      console.log('values', values);
+    onSubmit: async (values) => {
+      try {
+        const response = await signUp({
+          name: values.name,
+          second_name: values.second_name,
+          company_name: values.company,
+          email: values.email,
+          password: values.confirm_password,
+        });
+        console.log('response', response);
+        if (response.status === 201) {
+          // toast.success('Profile has been successfully created!');
+          console.log('Profile has been successfully created!');
+          await router.push({
+            pathname: '/auth',
+          });
+          // yield navigate(SIGN_IN, { replace: true });
+          // yield put(setErrorMessage(''));
+        }
+      } catch (e: any) {
+        if (e.response.data.statusCode === 403) {
+          console.log(e.response.data.message)
+          // yield put(setErrorMessage(e.response.data.message));
+        } else {
+          console.log(e.message)
+          // toast.error(e.message);
+        }
+      }
       // registerUser(
       //   {
       //     name: values.name,
@@ -49,24 +79,20 @@ export default function RegisterForm() {
     errors,
     touched,
     handleSubmit,
-    isSubmitting,
     getFieldProps,
-    handleChange,
-    setFieldValue,
-    values,
     isValid,
   } = formik;
 
   return (
     <FormikProvider value={formik}>
-      <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
-        <Stack flexDirection="row" mb={2} gap={1}>
+      <Form autoComplete='off' noValidate onSubmit={handleSubmit}>
+        <Stack flexDirection='row' mb={2} gap={1}>
           <Stack>
             <TextField
               fullWidth
               required
-              FormHelperTextProps={{style: {fontSize: 12}}}
-              placeholder="Name*"
+              FormHelperTextProps={{ style: { fontSize: 12 } }}
+              placeholder='Name*'
               {...getFieldProps('name')}
               error={Boolean(touched.name && errors.name)}
               helperText={touched.name && errors.name}
@@ -79,8 +105,8 @@ export default function RegisterForm() {
             <TextField
               fullWidth
               required
-              FormHelperTextProps={{style: {fontSize: 12}}}
-              placeholder="Surname*"
+              FormHelperTextProps={{ style: { fontSize: 12 } }}
+              placeholder='Surname*'
               {...getFieldProps('second_name')}
               error={Boolean(touched.second_name && errors.second_name)}
               helperText={touched.second_name && errors.second_name}
@@ -94,17 +120,17 @@ export default function RegisterForm() {
           <TextField
             fullWidth
             required
-            FormHelperTextProps={{style: {fontSize: 12}}}
-            placeholder="Company Name*"
+            FormHelperTextProps={{ style: { fontSize: 12 } }}
+            placeholder='Company Name*'
             {...getFieldProps('company')}
-            // error={
-            //   Boolean(touched.company && errors.company) ||
-            //   (errorMessage.includes('Organization') && errorMessage)
-            // }
-            // helperText={
-            //   Boolean(touched.company && errors.company) ||
-            //   (errorMessage.includes('Organization') && errorMessage)
-            // }
+            error={
+              Boolean(touched.company && errors.company)
+              // || (errorMessage.includes('Organization') && errorMessage)
+            }
+            helperText={
+              Boolean(touched.company && errors.company) ||
+              (errorMessage.includes('Organization') && errorMessage)
+            }
             InputLabelProps={{
               shrink: true,
             }}
@@ -112,18 +138,19 @@ export default function RegisterForm() {
           <TextField
             fullWidth
             required
-            type="email"
-            placeholder="Email*"
-            FormHelperTextProps={{style: {fontSize: 12}}}
+            type='email'
+            placeholder='Email*'
+            FormHelperTextProps={{ style: { fontSize: 12 } }}
             {...getFieldProps('email')}
-            // error={
-            //   Boolean(touched.email && errors.email) ||
-            //   (errorMessage.includes('Admin') && errorMessage)
-            // }
-            // helperText={
-            //   Boolean(touched.email && errors.email) ||
-            //   (errorMessage.includes('Admin') && errorMessage)
-            // }
+            error={
+              Boolean(touched.email && errors.email)
+              // ||
+              // (errorMessage.includes('Admin') && errorMessage)
+            }
+            helperText={
+              Boolean(touched.email && errors.email) ||
+              (errorMessage.includes('Admin') && errorMessage)
+            }
             InputLabelProps={{
               shrink: true,
             }}
@@ -131,14 +158,14 @@ export default function RegisterForm() {
           <TextField
             fullWidth
             type={showPassword ? 'text' : 'password'}
-            placeholder="Password*"
-            FormHelperTextProps={{style: {fontSize: 12}}}
+            placeholder='Password*'
+            FormHelperTextProps={{ style: { fontSize: 12 } }}
             {...getFieldProps('password')}
             InputProps={{
               endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={handleShowPassword} edge="end">
-                    <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'}/>
+                <InputAdornment position='end'>
+                  <IconButton onClick={handleShowPassword} edge='end'>
+                    <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
                   </IconButton>
                 </InputAdornment>
               ),
@@ -152,17 +179,17 @@ export default function RegisterForm() {
           <TextField
             fullWidth
             type={showSecondPassword ? 'text' : 'password'}
-            placeholder="Confirm password*"
+            placeholder='Confirm password*'
             {...getFieldProps('confirm_password')}
-            FormHelperTextProps={{style: {fontSize: 12}}}
+            FormHelperTextProps={{ style: { fontSize: 12 } }}
             InputLabelProps={{
               shrink: true,
             }}
             InputProps={{
               endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={handleShowSecondPassword} edge="end">
-                    <Iconify icon={showSecondPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'}/>
+                <InputAdornment position='end'>
+                  <IconButton onClick={handleShowSecondPassword} edge='end'>
+                    <Iconify icon={showSecondPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
                   </IconButton>
                 </InputAdornment>
               ),
@@ -172,16 +199,16 @@ export default function RegisterForm() {
           />
         </Stack>
 
-        <Button fullWidth type="submit" disabled={!isValid} variant="contained"
-                       style={{fontSize: '16px', fontFamily: 'Outfit'}}>
+        <Button fullWidth type='submit' disabled={!isValid} variant='contained'
+                style={{ fontSize: '16px', fontFamily: 'Outfit' }}>
           Sign up
         </Button>
-        <Typography fontSize="12px" color="#4B4A4A" fontWeight='600' fontFamily="Outfit" align="center" sx={{mt: 3}}>
+        <Typography fontSize='12px' color='#4B4A4A' fontWeight='600' fontFamily='Outfit' align='center' sx={{ mt: 3 }}>
           Already have an account?{' '}
           <Link
             // component={RouterLink}
             // to={SIGN_IN}
-            underline="always"
+            underline='always'
             style={{
               fontFamily: 'Outfit',
               fontStyle: 'normal',
@@ -189,7 +216,7 @@ export default function RegisterForm() {
               fontSize: '12px',
               lineHeight: '15px',
               color: '#000000',
-              textDecoration: 'none'
+              textDecoration: 'none',
             }}
           >
             Sign In
@@ -199,18 +226,3 @@ export default function RegisterForm() {
     </FormikProvider>
   );
 }
-
-// const SignInLink = styled(Link)({
-//   fontFamily: 'Outfit',
-//   fontStyle: 'normal',
-//   fontWeight: 600,
-//   fontSize: '12px',
-//   lineHeight: '15px',
-//   color: '#000000',
-//   textDecoration: 'none'
-// })
-
-// const mapStateToProps = (state) => ({state});
-// const mapActionsToProps = {registerUser};
-//
-// export default connect(mapStateToProps, mapActionsToProps)(RegisterForm);
