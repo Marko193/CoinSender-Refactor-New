9c388bbdef19d84ff39fb5d95a2bed462603513f
Marko Pavlenko <mark.pavlenko@megadevllc.com>
2023-04-03 19:06:03 +0300

    feat: made logout api functionality

--- helpers/api/auth.ts
+++ helpers/api/auth.ts
@@ -1,4 +1,7 @@
 // @ts-ignore
+import { logout } from '@/services';
+import Router from 'next/router';
+
 export const setDataToLocalStorage = (key: any, value: any) => {
   localStorage.setItem(key, value);
 };
@@ -11,11 +14,28 @@ export const removeDataFromLocalstorage = (key: any) => {
 // @ts-ignore
 export const getCookie = (cookieName: any) => {
   let cookie = {};
-  document.cookie.split(';').forEach(function (el) {
+  document.cookie.split(';').forEach(function(el) {
     let [key, value] = el.split('=');
     // @ts-ignore
     cookie[key.trim()] = value;
   });
   // @ts-ignore
   return cookie[cookieName];
+};
+
+export const logoutFunction = async () => {
+  // remove user from local storage and redirect to login page
+  try {
+    const response: any = await logout();
+    if (response.status === 200) {
+      console.log('logout');
+      await Router.push('/auth');
+      localStorage.removeItem('authorization_login');
+      localStorage.removeItem('access_token');
+      localStorage.removeItem('currentUser');
+      localStorage.removeItem('refresh_token');
+    }
+  } catch (error: any) {
+    console.log('error', error.message);
+  }
 };
